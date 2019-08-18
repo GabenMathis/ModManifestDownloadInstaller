6974f6c6854ae1050140def2473ce49f4aa411b1
Gaben Mathis <Gaben.Mathis@gmail.com>
2019-08-18 20:03:30 +1000

    Seperate main modules into individual files

---
+++ MMDI/ModManifestDownloadInstaller/SettingsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModManifestDownloadInstaller
+{
+    /// <summary>
+    /// Interaction logic for SettingsPage.xaml
+    /// </summary>
+    public partial class SettingsPage : Page
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public SettingsPage()
+        {
+            InitializeComponent();
+
+            Logger.Trace("Load SettingsPage");
+        }
+    }
+}
